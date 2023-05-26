a4d302a1ade9a9d7d56f4b27e07b0a2ace66172a
fatimabatool 2303-001-KHI-CND <fatima.batool@xloopdigital.com>
2023-05-26 03:58:25 -0700

    XSS and SQL injection done

--- src/main/java/com/emeritus/org/service/impl/StudentServiceImpl.java
+++ src/main/java/com/emeritus/org/service/impl/StudentServiceImpl.java
@@ -35,23 +35,10 @@ public class StudentServiceImpl implements StudentService{
 	}
 
 	@Override
-	public Student getStudentById(String id)  {
-		String queryStr = "SELECT "+id +", email, first_name as firstName, last_name as lastName FROM students WHERE id = " + id;
-		Query query = entityManager.createNativeQuery(queryStr);
-		List results = query.getResultList();
-		if(results.isEmpty()){
-			System.out.println(results);
-		}
-		Object result = results.get(0);
-
-		Student student = new Student();
-		student.setId((Long) ((Object[]) result)[0]);
-		student.setEmail(((Object[]) result)[1].toString());
-		student.setFirstName(((Object[]) result)[2].toString());
-		student.setLastName(((Object[]) result)[3].toString());
-		return student;
-
+	public Student getStudentById(Long id)  {
+		return studentRepository.findById(id).orElse(null);
 	}
+
 
 	@Override
 	public Student updateStudent(Student student) {
